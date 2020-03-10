193d82d6c32e1d77ec71f7ce097b6cc754abd40c
Friska Sylvia <32946884+friskasylviaauliasari17@users.noreply.github.com>
2020-03-10 08:52:01 +0700

    upload pertama
    upload pertama

---
+++ application/views/login.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<link rel="stylesheet" type="text/css" href="bootstrap/css/bootstrap.min.css">
+</head>
+<body>
+
+<div class="container">
+<br>
+	<center>
+		<div class="col-md-5 col-md-offset-3">
+			<div class="panel panel-success">
+				<div class="panel-heading">
+					<h2>
+					<span class="glyphicon glyphicon-cutlery" aria-hidden="true"></span>  My Restourant
+					</h2>
+					<h3>Halaman Login</h3>
+					<p><span class="glyphicon glyphicon-road" aria-hidden="true"></span>   Jalan Kenangan 122, Jepara</p>
+					</div>
+					<br>
+					<div class="panel-body">
+							<form action="" method="post">
+								<div class="input-group">
+									<span class="input-group-addon" id="username">Username</span>
+                					<input type="text" name="username" class="form-control" placeholder="Username" aria-describedby="username" required>
+             					</div>
+              					<br>
+	              					<div class="input-group">
+	                				<span class="input-group-addon" id="password">Password</span>
+	               					<input type="password" name="password" class="form-control" placeholder="Password" aria-describedby="password" required>
+	              					</div>
+	              					<br>
+	              					<br>
+	              					<div>
+	                				<input type="submit" class="btn btn-info btn-block" name="flogin" value="Login">
+	                			</div>
+	                		</form>
+	                	</div>
+	                </div>
+	            </div>
+	        </div>
+		</center>
+
+</body>
+</html>
